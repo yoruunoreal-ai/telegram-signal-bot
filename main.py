ae0455a4de0906185ef33b364df44fcae03b6858
yoruunoreal-ai <yoruunoreal@gmail.com>
2026-03-17 09:44:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from telethon import TelegramClient, events
 import re
 import os
+from flask import Flask
+import threading
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
@@ -9,6 +11,16 @@ source_group = -3026686847
 target_group = -3780005944
 
 client = TelegramClient("session", api_id, api_hash)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot attivo"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 def format_signal(text):
     text = re.sub(r'[^\x00-\x7F]+', '', text)
@@ -37,5 +49,8 @@ async def forward_signal(event):
         await client.send_message(target_group, new_text)
 
 print("Bot avviato...")
+
+threading.Thread(target=run_web).start()
+
 client.start()
 client.run_until_disconnected()
